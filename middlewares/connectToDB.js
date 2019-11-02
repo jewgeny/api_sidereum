e658a1b765e5573b2d307d305c00c211cbcd8e8f
Eugen Kuhn <eugenkuhn@eugens-imac.localdomain>
2019-11-02 23:32:35 +0100

    add html file

--- middlewares/connectToDB.js
+++ middlewares/connectToDB.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const dotenf = require("dotenv").config();
 const url = process.env.MONGO_DB;
 
-const connectToDB = async () => {
+
+const connectToDB = async (req,res) => {
      try{
         console.log("Before connecting to Atlas")
         await mongoose.connect(url, 
@@ -10,7 +11,8 @@ const connectToDB = async () => {
         console.log("Connected to Atlas");
      }
      catch(error){ 
+         console.log(error)
+     }
 }
 
 module.exports = connectToDB;
-
--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require("express");
 const nodemon = require("nodemon");
 const app = express();
 //const port = 4000;
-const port = env.process.PORT || 4000;
+const dotenf = require("dotenv").config();
+const port = process.env.PORT || 4000;
 const connectToDB = require("./middlewares/connectToDB");
 const saergeRouter = require("./routes/products/saerge");
+var path = require('path');
+
+app.get('/', function(req, res) {
+     res.sendFile(path.join(__dirname + '/index.html'));
+ });
+
+
+
+
+
 
 app.use("/saerge", saergeRouter);
 
