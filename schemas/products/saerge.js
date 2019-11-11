336db8b2fc4ba954cb8b11f32dfc71d1321b1355
Eugen Kuhn <eugenkuhn@eugens-imac.localdomain>
2019-11-11 17:00:41 +0100

    change type of price to Number

--- schemas/products/saerge.js
+++ schemas/products/saerge.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const productSaerge = new mongoose.Schema({
     img: {type: String, required: false},
     titel: {type: String, required: false},
-    price: {type: String, required: false},
+    price: {type: Number, required: false},
     category: {type: String, required: false},
     art_nr: {type: String, required: false},
    
