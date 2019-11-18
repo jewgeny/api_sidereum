26696bb8be10b4f85d03bd229fce8e8844428b48
Eugen Kuhn <eugenkuhn@eugens-imac.localdomain>
2019-11-18 13:52:24 +0100

    add update function

--- middlewares/products/saerge.js
+++ middlewares/products/saerge.js
@@ -1,4 +1,11 @@
 const sargeModel = require("../../models/products/saerge");
+const multer = require("multer"); //for upload file
+const GridFsStorage = require("multer-gridfs-storage"); //for connecting to mongoDB
+const Grid = require("gridfs-stream"); //Easily stream files to and from MongoDB GridFS
+const path = require("path"); //path
+const crypto = require("crypto");
+const url = process.env.MONGO_DB;
+//const methodOverride = require("method-override");
 
 
 const getSaerge = async (req, res) => {
@@ -24,7 +31,31 @@ const deleteSarg = async(req, res) => {
    }
 }
 
+
+
+var storage = new GridFsStorage({
+  url: sargeModel,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
+
 const createSaerge = async (req, res) => {
+
      try{
          await sargeModel.create({
                img: req.body.img,
@@ -41,6 +72,17 @@ const createSaerge = async (req, res) => {
      }
 }
 
+const updateSarg = async (req, res)  => {
+     let id = req.params.id;
+     try{
+         await sargeModel.findOneAndUpdate({_id: id},{ $set: req.body}, {new: true});
+         res.status(202).json({msg: "update was successfully"});
+     }
+     catch(err){
+        console.log(err)
+     }
+}
 
 
-module.exports = {getSaerge, createSaerge, deleteSarg}
+
+module.exports = {getSaerge, createSaerge, deleteSarg, updateSarg}
--- routes/products/saerge.js
+++ routes/products/saerge.js
@@ -1,9 +1,10 @@
-const {getSaerge, createSaerge, deleteSarg} = require("../../middlewares/products/saerge");
+const {getSaerge, createSaerge, deleteSarg, updateSarg} = require("../../middlewares/products/saerge");
 const express = require("express");
 const saergeRouter = express.Router();
 
 saergeRouter.get("/getData", getSaerge);
 saergeRouter.post("/create", createSaerge);
 saergeRouter.delete("/delete", deleteSarg);
+saergeRouter.put("/update/:id", updateSarg);
 
 module.exports = saergeRouter;
